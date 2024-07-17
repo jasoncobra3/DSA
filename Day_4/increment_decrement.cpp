326d4cee9c39c4f49cbb3740467f26cf73582ed4
Aniket <127662135+jasoncobra3@users.noreply.github.com>
2024-07-17 22:44:40 +0530

    Day-4

---
+++ Day_4/increment_decrement.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=9;
+    cout<<(++i)<<endl;
+    // 8
+    cout<<(i++)<<endl;
+    // 7,8
+    cout<<(--i)<<endl;
+    // 6
+    cout<<(i--)<<endl;
+    // 7,6
+
+}
